2df21b0284fae47744a6a03002b310569120a229
Khalidashraf141 <zerosimonriley833@gmail.com>
2026-01-05 13:19:59 +0530

    new update

---
+++ 14 Standard Template Library/DRY-RUN EXERCISES/program_14_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int U[] = {10, 20, 30, 40, 50, 60, 70, 80,90, 100};
+    int X[] = {10, 30, 50, 70, 90};
+    vector<int> VZ(10);
+    vector<int>::iterator itr;
+    itr = set_difference(U,U+10,X,X+5,VZ.begin());
+    VZ.resize(itr - VZ.begin());
+    for(int i = 0; i < VZ.size(); i++)
+        cout << VZ[i] << " ";
+    return 0;
+}
